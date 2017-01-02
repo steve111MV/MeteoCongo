eea4edf20d80f35734a362f57d723c973a2d239b
steve111MV <unlivregratuit@gmail.com>
2017-01-02 18:30:08 +0100

    Seted forecast list item textColor to "dark grey" as the App default Theme is now set to Dark

--- app/src/main/java/cg/stevendende/sunshine/tools/WeatherDataParser.java
+++ app/src/main/java/cg/stevendende/sunshine/tools/WeatherDataParser.java
@@ -9,7 +9,10 @@ import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 
 /**
- * Created by STEVEN on 06/09/2016.
+ * Created by @steve111MV on 06/09/2016.
+ *
+ * The content of this parser is originaly from Github & tailored to my needs
+ * https://gist.github.com/udacityandroid/4ee49df1694da9129af9
  */
 public class WeatherDataParser {
 
